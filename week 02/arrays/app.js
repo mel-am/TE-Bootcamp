0be518b54b3c0e45499aa2348dd344e215740493
mel-am <122634403+mel-am@users.noreply.github.com>
2024-03-25 15:55:17 +0000

    Add files via upload

---
+++ week 02/arrays/app.js
@@ -0,0 +1,61 @@
+
+console.log("Hello World!");
+
+
+const blogPosts = [
+    "My first blog post", "My second blog post", "My third blog post"
+];
+
+console.log(blogPosts)
+
+
+//Accessing items
+
+const blogPosts2 = [
+    "My first1 blog post",
+    "My second2 blog post",
+    "My third3 blog post",
+
+];
+console.log(blogPosts2);
+
+const blogPosts3 = [
+    "My first blog post",
+    "My second blog post",
+    "My third blog post",
+];
+  
+blogPosts3[3] = "My fourth blog post";
+
+console.log(blogPosts[3]); // Output: My fourth blog post
+
+const favouriteFoods = [ //favourite foods
+    "Pancakes",
+    "Fish",
+    "Apple",
+    "Chicken",
+    "Pasta",
+
+
+];
+console.log(favouriteFoods); 
+
+const favouriteColours = [ //favourite colours
+    "Cyan",
+    "Green",
+    "Purple",
+    "Orange",
+
+];
+console.log(favouriteColours); 
+
+const favouriteNumbers = [ //favourite numbers
+    8,
+    15,
+    180,
+    243,
+    7565,
+    442,
+
+];
+console.log(favouriteNumbers); 
